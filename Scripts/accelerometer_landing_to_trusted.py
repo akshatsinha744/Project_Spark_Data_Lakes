c686df6405d68d06961d65a87e85fb67482d6a3f
akshatsinha744 <96653938+akshatsinha744@users.noreply.github.com>
2025-02-17 16:45:15 +0530

    Update accelerometer_landing_to_trusted.py

--- Scripts/accelerometer_landing_to_trusted.py
+++ Scripts/accelerometer_landing_to_trusted.py
@@ -12,32 +12,32 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node customer_trusted
-customer_trusted_node1689563329110 = glueContext.create_dynamic_frame.from_catalog(
+# Load data from customer_trusted table
+customer_trusted_node = glueContext.create_dynamic_frame.from_catalog(
     database="dend",
     table_name="customer_trusted",
-    transformation_ctx="customer_trusted_node1689563329110",
+    transformation_ctx="customer_trusted_node",
 )
 
-# Script generated for node accelerometer_landing
-accelerometer_landing_node1 = glueContext.create_dynamic_frame.from_catalog(
+# Load data from accelerometer_landing table
+accelerometer_landing_node = glueContext.create_dynamic_frame.from_catalog(
     database="dend",
     table_name="accelerometer_landing",
-    transformation_ctx="accelerometer_landing_node1",
+    transformation_ctx="accelerometer_landing_node",
 )
 
-# Script generated for node Customer Privacy Filter
-CustomerPrivacyFilter_node1689563552518 = Join.apply(
-    frame1=accelerometer_landing_node1,
-    frame2=customer_trusted_node1689563329110,
+# Apply Customer Privacy Filter
+CustomerPrivacyFilter_node = Join.apply(
+    frame1=accelerometer_landing_node,
+    frame2=customer_trusted_node,
     keys1=["user"],
     keys2=["email"],
-    transformation_ctx="CustomerPrivacyFilter_node1689563552518",
+    transformation_ctx="CustomerPrivacyFilter_node",
 )
 
-# Script generated for node Drop Fields
-DropFields_node1689563733947 = DropFields.apply(
-    frame=CustomerPrivacyFilter_node1689563552518,
+# Drop unnecessary fields
+DropFields_node = DropFields.apply(
+    frame=CustomerPrivacyFilter_node,
     paths=[
         "customername",
         "email",
@@ -50,19 +50,21 @@ DropFields_node1689563733947 = DropFields.apply(
         "birthday",
         "sharewithfriendsasofdate",
     ],
-    transformation_ctx="DropFields_node1689563733947",
+    transformation_ctx="DropFields_node",
 )
 
-# Script generated for node S3 bucket
-S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=DropFields_node1689563733947,
+# Enable dynamic schema inference and update Data Catalog
+S3bucket_node = glueContext.write_dynamic_frame.from_options(
+    frame=DropFields_node,
     connection_type="s3",
     format="json",
     connection_options={
         "path": "s3://dend-lake-house/accelerometer/trusted/",
         "partitionKeys": [],
+        "enableUpdateCatalog": True,
+        "updateBehavior": "UPDATE_IN_DATABASE"
     },
-    transformation_ctx="S3bucket_node3",
+    transformation_ctx="S3bucket_node",
 )
 
 job.commit()
